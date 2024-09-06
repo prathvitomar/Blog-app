a5a291232a184c365798b8cc7191b7355dbac5f3
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-09-06 12:37:51 +0530

    Created Config file again by myself for practice

--- blog-app/src/appwrite/config.js
+++ blog-app/src/appwrite/config.js
@@ -1,15 +1,16 @@
-import conf from '../conf/conf.js';
+import conf from '../conf/conf';
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service{
+export class Service {
     client = new Client();
-    databases;
-    bucket;
-    
+    databases
+    bucket
+
     constructor(){
         this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
+        .setEndpoint(conf.appWriteUrl)
+        .setProject(conf.appWriteProjectId)
+
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
@@ -17,15 +18,11 @@ export class Service{
     async createPost({title, slug, content, featuredImage, status, userId}){
         try {
             return await this.databases.createDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
                 slug,
                 {
-                    title,
-                    content,
-                    featuredImage,
-                    status,
-                    userId,
+                    title, content, featuredImage, status, userId
                 }
             )
         } catch (error) {
@@ -33,18 +30,15 @@ export class Service{
         }
     }
 
+    
     async updatePost(slug, {title, content, featuredImage, status}){
         try {
             return await this.databases.updateDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
                 slug,
                 {
-                    title,
-                    content,
-                    featuredImage,
-                    status,
-
+                    title, content, featuredImage, status
                 }
             )
         } catch (error) {
@@ -52,43 +46,40 @@ export class Service{
         }
     }
 
+    
     async deletePost(slug){
         try {
-            await this.databases.deleteDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
+            return await this.databases.deleteDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
                 slug
-            
             )
-            return true
         } catch (error) {
             console.log("Appwrite serive :: deletePost :: error", error);
-            return false
         }
     }
 
     async getPost(slug){
         try {
             return await this.databases.getDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug
-            
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
             )
         } catch (error) {
             console.log("Appwrite serive :: getPost :: error", error);
-            return false
         }
     }
 
-    async getPosts(queries = [Query.equal("status", "active")]){
+    
+    async getPosts(){
         try {
             return await this.databases.listDocuments(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                queries,
-                
-
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                [
+                    Query.equal("status", "active")
+                ]
             )
         } catch (error) {
             console.log("Appwrite serive :: getPosts :: error", error);
@@ -96,12 +87,11 @@ export class Service{
         }
     }
 
-    // file upload service
 
     async uploadFile(file){
         try {
-            return await this.bucket.createFile(
-                conf.appwriteBucketId,
+            return await this.databases.createFile(
+                conf.appWriteBucketId,
                 ID.unique(),
                 file
             )
@@ -114,7 +104,7 @@ export class Service{
     async deleteFile(fileId){
         try {
             await this.bucket.deleteFile(
-                conf.appwriteBucketId,
+                conf.appWriteBucketId,
                 fileId
             )
             return true
@@ -124,14 +114,16 @@ export class Service{
         }
     }
 
+
     getFilePreview(fileId){
-        return this.bucket.getFilePreview(
-            conf.appwriteBucketId,
-            fileId
-        )
+            return this.databases.getFilePreview(
+                conf.appWriteBucketId,
+                fileId
+            )    
     }
+    
 }
 
+const service = new Service();
 
-const service = new Service()
-export default service
+export default service;
