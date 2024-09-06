ca0694f6d5dc007cc617d426522ee10e9e25856f
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-09-06 17:43:10 +0530

    Created Header, Footer,Buttons,Input Components

--- blog-app/src/components/index.js
+++ blog-app/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
+import Button from './Button'
+import Input from './Input'
+import Logo from './Input'
 
 export {
     Header,
-    Footer
+    Footer,
+    Button,
+    Input,
+    Logo
 }
