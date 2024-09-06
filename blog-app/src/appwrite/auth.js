6537e487cf88fd13ffe4df0dd134365f3dac161f
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-09-06 13:16:29 +0530

    Connected Authentication with Redux state for auth

--- blog-app/src/appwrite/auth.js
+++ blog-app/src/appwrite/auth.js
@@ -8,8 +8,8 @@ export class AuthService {
 
     constructor() {
         this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId);
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.appWriteProjectId);
         this.account = new Account(this.client);
             
     }
