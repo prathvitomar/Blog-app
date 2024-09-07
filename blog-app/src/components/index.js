fb9971687e4445729bcaebf91bbe44e894b13c07
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-09-07 13:05:22 +0530

    Created Pages for Signup and login, and mentioned all component in index.js

--- blog-app/src/components/index.js
+++ blog-app/src/components/index.js
@@ -3,11 +3,21 @@ import Footer from './Footer/Footer'
 import Button from './Button'
 import Input from './Input'
 import Logo from './Input'
+import RTE from './RTE'
+import LogoutBtn from './Header/LogoutBtn'
+import Login from './Login'
+import Signup from './Signup'
+import PostForm from './post-form/PostForm'
 
 export {
     Header,
     Footer,
     Button,
     Input,
-    Logo
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm
 }
